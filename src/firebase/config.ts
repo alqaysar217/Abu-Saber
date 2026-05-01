f2232e814f51a590341f6cd870142ad6789e4d03
Mahmoud Hassani <pr.mahmoud.20@gmail.com>
2026-05-01 08:14:46 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,8 @@
-'use client';
-
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyDummyKey",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "abu-saber-fish.firebaseapp.com",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "abu-saber-fish",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "abu-saber-fish.appspot.com",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "123456789",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:123456789:web:abcdef"
+  "projectId": "studio-6954625944-92df9",
+  "appId": "1:117459018098:web:e4efef2b0d3c1823f4fdeb",
+  "apiKey": "AIzaSyAkJ1S637mtGgfLV-hv7IwBxK9_bDlaWGA",
+  "authDomain": "studio-6954625944-92df9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "117459018098"
 };
