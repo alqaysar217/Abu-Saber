fd08e2a4cbce4ac80b812a2defac09280a2df6fe
Mahmoud Hassani <pr.mahmoud.20@gmail.com>
2026-05-01 08:30:52 +0000

    404 This page could not be found. (_for element div_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,6 @@ export default function Home() {
     debtsByMe: false,
   })
 
-  // Prevent hydration errors by ensuring we only render browser-specific logic after mount
   useEffect(() => {
     setMounted(true)
   }, [])
